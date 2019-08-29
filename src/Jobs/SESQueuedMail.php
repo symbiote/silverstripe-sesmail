3b8a3276560ba3a3c067bdf4662e3bd38b748862
Will Rossiter <will@fullscreen.io>
2019-08-29 20:11:38 +1200

    Wrap in a class exists

--- src/Jobs/SESQueuedMail.php
+++ src/Jobs/SESQueuedMail.php
@@ -7,83 +7,86 @@ use Symbiote\QueuedJobs\Services\QueuedJob;
 use Exception;
 use SilverStripe\Core\Injector\Injector;
 
-/**
- * SESQueuedMail
- *
- * @author Stephen McMahon <stephen@symbiote.com.au>
- */
-class SESQueuedMail extends AbstractQueuedJob implements QueuedJob {
 
-	public function __construct($destinations = array(), $subject = '', $rawMessageText = '') {
-		if (!$destinations && !$subject && !$rawMessageText) {
-			// Avoid QueuedJobService::initialiseJob errors
-			return;
-		}
-		$this->To = $destinations;
-		$this->Subject = $subject;
-		$this->RawMessageText = $rawMessageText;
-	}
-
-	public function getTitle() {
-
-		return 'Email To: ' . implode(', ', $this->To) . ' Subject: ' . $this->Subject;
-	}
-
-	public function getSignature() {
-
-		return md5($this->Subject) . ' ' . implode(', ', $this->To);
-	}
-
-	public function getJobType() {
-
-		$this->totalSteps = '1';
-
-		return QueuedJob::QUEUED;
-	}
-
+if (class_exists(QueuedJob::class)) {
 	/**
-	 * Send this email. We try this only once and break as soon as something goes wrong to avoid sending multiple emails
-	 * or DoSing our job queued with slow processing emails.
+	 * SESQueuedMail
+	 *
+	 * @author Stephen McMahon <stephen@symbiote.com.au>
 	 */
-	public function process() {
+	class SESQueuedMail extends AbstractQueuedJob implements QueuedJob {
 
-		if ($this->isComplete) {
-			return;
+		public function __construct($destinations = array(), $subject = '', $rawMessageText = '') {
+			if (!$destinations && !$subject && !$rawMessageText) {
+				// Avoid QueuedJobService::initialiseJob errors
+				return;
+			}
+			$this->To = $destinations;
+			$this->Subject = $subject;
+			$this->RawMessageText = $rawMessageText;
 		}
-		$this->currentStep += 1;
 
-		// Detect issues with data
-		$isCorrupt = false;
-		$to = $this->To;
-		$rawMessageText = $this->RawMessageText;
-		if (!$to) {
+		public function getTitle() {
+
+			return 'Email To: ' . implode(', ', $this->To) . ' Subject: ' . $this->Subject;
+		}
+
+		public function getSignature() {
+
+			return md5($this->Subject) . ' ' . implode(', ', $this->To);
+		}
+
+		public function getJobType() {
+
+			$this->totalSteps = '1';
+
+			return QueuedJob::QUEUED;
+		}
+
+		/**
+		 * Send this email. We try this only once and break as soon as something goes wrong to avoid sending multiple emails
+		 * or DoSing our job queued with slow processing emails.
+		 */
+		public function process() {
+
+			if ($this->isComplete) {
+				return;
+			}
+			$this->currentStep += 1;
+
+			// Detect issues with data
+			$isCorrupt = false;
+			$to = $this->To;
+			$rawMessageText = $this->RawMessageText;
+			if (!$to) {
+				$this->addMessage('$this->To should not be empty.');
+				$isCorrupt = true;
+			}
+			if (!$rawMessageText) {
+				$this->addMessage('$this->RawMessageText should not be empty.');
+				$isCorrupt = true;
+			}
+			if ($isCorrupt) {
+				throw new Exception('Corrupted SESQueuedMail job (Missing "To" or "RawMessageText").', 'ERR');
+			}
+
+			$response = Injector::inst()->get(SESMailer::class)
+				->sendSESClient($to, $rawMessageText);
+
+			$this->addMessage('SES Response: '.print_r($response, true));
+
+			if (isset($response['MessageId']) && strlen($response['MessageId']) &&
+				(isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] == 200)) {
+				$this->RawMessageText = 'Email Sent Successfully. Message body deleted';
+				$this->isComplete = true;
+				return;
+			}
 			$this->addMessage('$this->To should not be empty.');
-			$isCorrupt = true;
-		}
-		if (!$rawMessageText) {
-			$this->addMessage('$this->RawMessageText should not be empty.');
-			$isCorrupt = true;
-		}
-		if ($isCorrupt) {
-			throw new Exception('Corrupted SESQueuedMail job (Missing "To" or "RawMessageText").', 'ERR');
+			if ($response) {
+				throw new Exception(json_encode($response->toArray()), 'ERR');
+			}
+			throw new Exception('Blank "response" result from SESMailer::sendSESClient()', 'ERR');
 		}
 
-		$response = Injector::inst()->get(SESMailer::class)
-			->sendSESClient($to, $rawMessageText);
-			
-		$this->addMessage('SES Response: '.print_r($response, true));
-
-		if (isset($response['MessageId']) && strlen($response['MessageId']) && 
-			(isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] == 200)) {
-			$this->RawMessageText = 'Email Sent Successfully. Message body deleted';
-			$this->isComplete = true;
-			return;
-		}
-		$this->addMessage('$this->To should not be empty.');
-		if ($response) {
-			throw new Exception(json_encode($response->toArray()), 'ERR');
-		}
-		throw new Exception('Blank "response" result from SESMailer::sendSESClient()', 'ERR');
 	}
-
 }
